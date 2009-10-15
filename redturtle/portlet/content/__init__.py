8e70b27b1bcc9c9a440ebc574a66cc1ddd1b3334
cekk <andrea.cecchi85@gmail.com>
2009-10-15 08:46:01 +0000

    Initial import.

--- redturtle/portlet/content/__init__.py
+++
@@ -1,5 +0,0 @@
-from zope.i18nmessageid import MessageFactory
-ContentPortletMessageFactory = MessageFactory('redturtle.portlet.content')
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
