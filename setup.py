d7eb7f542b4aa1a42d76e5edb54bf5d888d65eb8
Luca Fabbri <luca@keul.it>
2015-02-16 16:45:17 +0100

    egg infos fixes, nothing new

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ version = '1.4.1.dev0'
 
 setup(name='redturtle.portlet.content',
       version=version,
-      description="A generic portlet that allows to show various parts of an object, like the title, description, text, image and comments.",
+      description="A generic portlet that allows to show various parts of a document, "
+                  "like title, description, text, image and comments.",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -14,12 +15,14 @@ setup(name='redturtle.portlet.content',
         "Framework :: Plone :: 3.3",
         "Framework :: Plone :: 4.0",
         "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='portlet content generic',
       author='RedTurtle Technology',
-      author_email='sviluppoplone@redturtle.net',
+      author_email='sviluppoplone@redturtle.it',
       url='http://plone.org/products/redturtle.portlet.content',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
